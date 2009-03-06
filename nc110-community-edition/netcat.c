48ba26252bad91ae8f66dee0cd4817cf5b280a41
vapier <vapier@a893ff13-3927-0410-ad06-ee0adabe4a53>
2009-03-06 01:28:37 +0000

    do not define HAVE_BIND by default -- let builders do it

--- nc110-community-edition/netcat.c
+++ nc110-community-edition/netcat.c
@@ -31,7 +31,7 @@
 /* conditional includes -- a very messy section which you may have to dink
    for your own architecture [and please send diffs...]: */
 /* #undef _POSIX_SOURCE */	/* might need this for something? */
-#define HAVE_BIND		/* ASSUMPTION -- seems to work everywhere! */
+/* #define HAVE_BIND */		/* ASSUMPTION -- seems to work everywhere! */
 #define HAVE_HELP		/* undefine if you dont want the help text */
 /* #define ANAL */		/* if you want case-sensitive DNS matching */
 
